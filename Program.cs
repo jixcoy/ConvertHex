6c103db47e199e0df6b39479a3e27e71cb0a14eb
jixcoy <89876028+jixcoy@users.noreply.github.com>
2022-11-03 12:50:05 -0700

    the code lol

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace HexConverter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string teh = "hello world";
+            ConvertHex(teh);
+        }
+
+        public static void ConvertHex(string s)
+        {
+            foreach (byte b in Encoding.ASCII.GetBytes(s)) Console.Write(Convert.ToString(b, 16) + " ");
+        }
+    }
+}
